d37db0214952b1ba1fae49204dbf2b1572e0add6
David Junior <david.junior211204@gmail.com>
2026-04-08 23:56:40 +0000

    confirmado

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,4 +5,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/TW26/',
+  server: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
+  preview: {
+    host: true,
+    port: 5173,
+    strictPort: true,
+  },
 })
